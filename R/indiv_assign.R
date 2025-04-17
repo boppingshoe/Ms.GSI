d9c7a1d34343f12d52df19fba5cd4a5afa71674a
bhsu <bobby.hsu@alaska.gov>
2025-04-17 10:33:46 -0800

    updates

--- R/indiv_assign.R
+++ R/indiv_assign.R
@@ -15,9 +15,10 @@
 #'   baseline1_data = base_templin, baseline2_data = base_yukon,
 #'   pop1_info = templin_pops211, pop2_info = yukon_pops50, sub_group = 3:5)
 #'
+#' # run model
 #' msgsi_out <- msgsi_mdl(msgsi_dat, nreps = 25, nburn = 15, thin = 1, nchains = 1)
 #'
-#' # trace plot
+#' # individual assignment summary
 #' ind_iden <- indiv_assign(msgsi_out, msgsi_dat)
 #'
 indiv_assign <- function(mdl_out, mdl_dat) {
